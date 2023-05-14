7342a3ae79bb6cc413324af379c3705127da0fac
Oguz Vuruskaner <ovuruska@outlook.com>
2023-05-14 12:30:05 +0300

    Removed .js files

--- src/Configurations/Providers/swagger-provider.ts
+++ src/Configurations/Providers/swagger-provider.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+  import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function SwaggerProvider(app: INestApplication): void {
